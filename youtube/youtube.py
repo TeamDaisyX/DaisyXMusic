0c4c61d4d40861330999690ea0149b756a190074
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-02 14:25:27 +0530

    Update youtube.py

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -18,7 +18,7 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+            f"**Hêllẞø†:** Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
         )
 
     ydl.download([url])
