3daa77b4a62e73dd8e5a7d18b87a1a642f172b61
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-16 14:54:56 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -5,7 +5,7 @@ import tgcalls
 from converter import convert
 from youtube import download
 import sira
-from config import DURATION_LIMIT
+from config import DURATION_LIMIT, BOT_NAME as BN
 from helpers.wrappers import errors
 from helpers.errors import DurationLimitError
 
@@ -16,7 +16,7 @@ from helpers.errors import DurationLimitError
     & ~ filters.edited
 )
 async def play_(client: Client, message: Message):
-    await message.reply_text("**Hêllẞø†:** Sorry! I can only be used in groups. \nTry again in a group.")
+    await message.reply_text(f"**{BN} :** Sorry! I can only be used in groups. \nTry again in a group.")
 
 
 @Client.on_message(
@@ -28,12 +28,12 @@ async def play_(client: Client, message: Message):
 async def play(client: Client, message_: Message):
     audio = (message_.reply_to_message.audio or message_.reply_to_message.voice) if message_.reply_to_message else None
 
-    res = await message_.reply_text("**Hêllẞø†:** 🔄 Processing...")
+    res = await message_.reply_text(f"**{BN} :** 🔄 Processing...")
 
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
             raise DurationLimitError(
-                f"**Hêllẞø†:** Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {audio.duration / 60} minute(s)"
+                f"**{BN} :** Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {audio.duration / 60} minute(s)"
             )
 
         file_name = audio.file_id + audio.file_name.split(".")[-1]
@@ -59,7 +59,7 @@ async def play(client: Client, message_: Message):
                         break
 
         if offset == None:
-            await res.edit_text("**Hêllẞø†:**❕ You did not give me anything to play.")
+            await res.edit_text(f"**{BN} :**❕ You did not give me anything to play.")
             return
 
         url = text[offset:offset+length]
@@ -73,7 +73,7 @@ async def play(client: Client, message_: Message):
 
     if is_playing:
         position = await sira.add(message_.chat.id, file_path)
-        await res.edit_text(f"**Hêllẞø†:** #️⃣ Queued at position {position}.")
+        await res.edit_text(f"**{BN} :** #️⃣ Queued at position {position}.")
     else:
-        await res.edit_text("**Hêllẞø†:** ▶️ Playing...")
+        await res.edit_text(f"**{BN} :** ▶️ Playing...")
         tgcalls.pytgcalls.join_group_call(message_.chat.id, file_path, 48000)
