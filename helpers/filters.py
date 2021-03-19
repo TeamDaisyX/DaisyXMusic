e0f5addccb8aa307a472dd94eef2488b9c90d34b
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-20 04:32:05 +0530

    Add files via upload

---
+++ helpers/filters.py
@@ -0,0 +1,13 @@
+from typing import Union, List
+
+from pyrogram import filters
+
+from config import COMMAND_PREFIXES
+
+other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+
+
+def command(commands: Union[str, List[str]]):
+    return filters.command(commands, COMMAND_PREFIXES)
+
