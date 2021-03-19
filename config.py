5eb3f9c7b0f5793f86987b2f02c8b0d066fb7af1
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-20 04:40:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
