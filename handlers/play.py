f5f76b53c89ad2b996f913818be34178d53eeef5
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-27 03:01:08 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -8,64 +8,37 @@ import callsmusic
 import converter
 from downloaders import youtube
 
-from config import BOT_NAME as BN, DURATION_LIMIT
-from helpers.errors import DurationLimitError
+from config import BOT_NAME as bn, DURATION_LIMIT
 from helpers.filters import command, other_filters
 from helpers.decorators import errors
+from helpers.errors import DurationLimitError
+from helpers.gets import get_url, get_file_name
 
 
 @Client.on_message(command("play") & other_filters)
 @errors
 async def play(_, message: Message):
     audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
-
-    res = await message.reply_text(f"**{BN} :-** ⏳ Processing...")
+    url = get_url(message)
 
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
             raise DurationLimitError(
-                f"**{BN} :-** 😕 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {audio.duration / 60} minute(s)"
+                f"**{bn} :-** 😕 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed!\n🤐 The provided video is {audio.duration / 60} minute(s)"
             )
 
-        file_name = audio.file_unique_id + "." + (
-            audio.file_name.split(
-                ".")[-1] if not isinstance(audio, Voice) else "ogg"
-        )
+        file_name = get_file_name(audio)
         file_path = await converter.convert(
             (await message.reply_to_message.download(file_name))
             if not path.isfile(path.join("downloads", file_name)) else file_name
         )
-    else:
-        messages = [message]
-        text = ""
-        offset = None
-        length = None
-
-        if message.reply_to_message:
-            messages.append(message.reply_to_message)
-
-        for _message in messages:
-            if offset:
-                break
-
-            if _message.entities:
-                for entity in _message.entities:
-                    if entity.type == "url":
-                        text = _message.text or _message.caption
-                        offset, length = entity.offset, entity.length
-                        break
-
-        if offset in (None,):
-            await res.edit_text(f"**{BN} :-** 🙄 You did not give me anything to play!")
-            return
-
-        url = text[offset:offset + length]
-
+    elif url:
         file_path = await converter.convert(youtube.download(url))
+    else:
+        return await message.reply_text(f"**{bn} :-** 🙄 You did not give me anything to play!")
 
     if message.chat.id in callsmusic.pytgcalls.active_calls:
-        position = callsmusic.queues.add(message.chat.id, file_path)
-        await res.edit_text(f"**{BN} :-** 💬 Queued at position #{position}!")
+        await message.reply_text(f"**{bn} :-** 😉 Queued at position #{callsmusic.queues.put(message.chat.id, file_path=file_path)}!")
     else:
-        await res.edit_text(f"**{BN} :-** 🥳 Playing...")
         callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
+        await message.reply_text(f"**{bn} :-** 🥳 Playing...")
