5b333f826a754159bdbcbded6c675f62ef1d8ba5
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-26 12:14:19 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -1,87 +1,76 @@
-import queue
+from asyncio.queues import QueueEmpty
 
 from pyrogram import Client
 from pyrogram.types import Message
 
 import callsmusic
 
-import queues
-import cache.admins
-
 from config import BOT_NAME as BN
-from helpers.filters import command
-from helpers.wrappers import errors, admins_only, nonadmin
+from helpers.filters import command, other_filters
+from helpers.decorators import errors, authorized_users_only
 
 
-@Client.on_message(command(["pause", "p"]))
+@Client.on_message(command("pause") & other_filters)
 @errors
-@admins_only
+@authorized_users_only
 async def pause(_, message: Message):
     if (
             message.chat.id not in callsmusic.pytgcalls.active_calls
     ) or (
             callsmusic.pytgcalls.active_calls[message.chat.id] == 'paused'
     ):
-        await message.reply_text(f"**{BN} :**❕ Nothing is playing.")
+        await message.reply_text(f"**{BN} :-** 🙄 Nothing is playing!")
     else:
         callsmusic.pytgcalls.pause_stream(message.chat.id)
-        await message.reply_text(f"**{BN} :** ⏸ Paused.")
+        await message.reply_text(f"**{BN} :-** 🤐 Paused!")
 
 
-@Client.on_message(command(["resume", "r"]))
+@Client.on_message(command("resume") & other_filters)
 @errors
-@admins_only
+@authorized_users_only
 async def resume(_, message: Message):
     if (
             message.chat.id not in callsmusic.pytgcalls.active_calls
     ) or (
             callsmusic.pytgcalls.active_calls[message.chat.id] == 'playing'
     ):
-        await message.reply_text(f"**{BN} :**❕ Nothing is paused.")
+        await message.reply_text(f"**{BN} :-** 🙄 Nothing is paused!")
     else:
         callsmusic.pytgcalls.resume_stream(message.chat.id)
-        await message.reply_text(f"**{BN} :** ▶️ Resumed.")
+        await message.reply_text(f"**{BN} :-** 🥳 Resumed!")
 
 
-@Client.on_message(command(["end", "s"]))
+@Client.on_message(command("stop") & other_filters)
 @errors
-@admins_only
+@authorized_users_only
 async def stop(_, message: Message):
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text(f"**{BN} :**❕ Nothing is streaming.")
+        await message.reply_text(f"**{BN} :-** 🙄 Nothing is streaming!")
     else:
         try:
-            queues.clear(message.chat.id)
-        except queue.Empty:
+            callsmusic.queues.clear(message.chat.id)
+        except QueueEmpty:
             pass
 
         callsmusic.pytgcalls.leave_group_call(message.chat.id)
-        await message.reply_text(f"**{BN} :** ⏹ Stopped streaming.")
+        await message.reply_text(f"**{BN} :-** ❌ Stopped streaming!")
 
 
-@Client.on_message(command(["skip", "next"]))
+@Client.on_message(command("skip") & other_filters)
 @errors
-@admins_only
+@authorized_users_only
 async def skip(_, message: Message):
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text(f"**{BN} :**❕ Nothing is playing to skip.")
+        await message.reply_text(f"**{BN} :-** 🙄 Nothing is playing to skip!")
     else:
-        queues.task_done(message.chat.id)
+        callsmusic.queues.task_done(message.chat.id)
 
-        if queues.is_empty(message.chat.id):
+        if callsmusic.queues.is_empty(message.chat.id):
             callsmusic.pytgcalls.leave_group_call(message.chat.id)
         else:
-            callsmusic.pytgcalls.change_stream(message.chat.id, queues.get(message.chat.id)["file_path"])
+            callsmusic.pytgcalls.change_stream(
+                message.chat.id,
+                callsmusic.queues.get(message.chat.id)["file_path"]
+            )
 
-        await message.reply_text(f"**{BN} :** ⏩ Skipped the current song.")
-
-
-@Client.on_message(command("admincache"))
-@errors
-@admins_only
-async def admincache(_, message: Message):
-    cache.admins.set(
-        message.chat.id,
-        [member.user for member in await message.chat.get_members(filter="administrators")]
-    )
-    await message.reply_text(f"**{BN} :** ❇ Admin cache refreshed!")
+        await message.reply_text(f"**{BN} :-** 😬 Skipped the current song!")
