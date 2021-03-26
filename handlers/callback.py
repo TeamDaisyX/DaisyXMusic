1878636cdaadff0b52895c261f2cf7c4e53869fb
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-26 12:16:51 +0530

    Delete callback.py

--- handlers/callback.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery
-
-
-@Client.on_callback_query(filters.regex("close"))
-async def close(_, query: CallbackQuery):
-    await query.message.delete()
