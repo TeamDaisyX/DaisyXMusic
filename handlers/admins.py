25fd2fed627594899c1c6acbad992ba33139f526
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-27 09:02:44 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -2,7 +2,7 @@ from asyncio.queues import QueueEmpty
 
 from pyrogram import Client
 from pyrogram.types import Message
-from callsmusic.handlers.play import file_path
+from handlers.play import file_path
 from callsmusic import callsmusic
 
 from config import BOT_NAME as BN
