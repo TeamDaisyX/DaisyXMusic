b7006d1beff1a30e442c656d7733fc8be7567283
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-27 09:47:07 +0530

    Update youtube.py

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -20,7 +20,7 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"**{bn} :-** 😕 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
         )
 
     ydl.download([url])
