a9550794a6c3732a0d85d6a08553808b2dda5955
Inuka ASiTH <inukaasith7@gmail.com>
2021-04-06 07:42:59 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -22,6 +22,78 @@ from helpers.errors import DurationLimitError
 from helpers.gets import get_url, get_file_name
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+import os
+import aiohttp
+import aiofiles
+import ffmpeg
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+
+def transcode(filename):
+    ffmpeg.input(filename).output("input.raw", format='s16le', acodec='pcm_s16le', ac=2, ar='48k').overwrite_output().run() 
+    os.remove(filename)
+
+# Convert seconds to mm:ss
+def convert_seconds(seconds):
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02d:%02d" % (minutes, seconds)
+
+
+# Convert hh:mm:ss to seconds
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+
+
+# Change image size
+def changeImageSize(maxWidth, maxHeight, image):
+    widthRatio = maxWidth / image.size[0]
+    heightRatio = maxHeight / image.size[1]
+    newWidth = int(widthRatio * image.size[0])
+    newHeight = int(heightRatio * image.size[1])
+    newImage = image.resize((newWidth, newHeight))
+    return newImage
+
+async def generate_cover(requested_by, title, views, duration, thumbnail):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(thumbnail) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open("background.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+
+    image1 = Image.open("./background.png")
+    image2 = Image.open("etc/foreground.png")
+    image3 = changeImageSize(1280, 720, image1)
+    image4 = changeImageSize(1280, 720, image2)
+    image5 = image3.convert("RGBA")
+    image6 = image4.convert("RGBA")
+    Image.alpha_composite(image5, image6).save("temp.png")
+    img = Image.open("temp.png")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("etc/font.otf", 32)
+    draw.text((190, 550), f"Title: {title}", (255, 255, 255), font=font)
+    draw.text(
+        (190, 590), f"Duration: {duration}", (255, 255, 255), font=font
+    )
+    draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
+    draw.text((190, 670),
+        f"Played By: {requested_by}",
+        (255, 255, 255),
+        font=font,
+    )
+    img.save("final.png")
+    os.remove("temp.png")
+    os.remove("background.png")
+
+
+
+
 @Client.on_message(command("play") & other_filters)
 @errors
 async def play(_, message: Message):
@@ -51,6 +123,23 @@ async def play(_, message: Message):
             )
 
         file_name = get_file_name(audio)
+        title = file_name
+        thumb_name = "https://telegra.ph/file/a4fa687ed647cfef52402.jpg"
+        thumbnail = thumb_name
+        duration = round(audio.duration / 60)
+        views = "Locally added"
+        keyboard = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="🔊 Channel",
+                            url=f"https://t.me/Daisyxupdates")
+
+                    ]
+                ]
+            )
+        requested_by = message.from_user.mention()
+        await generate_cover(requested_by, title, views, duration, thumbnail)  
         file_path = await converter.convert(
             (await message.reply_to_message.download(file_name))
             if not path.isfile(path.join("downloads", file_name)) else file_name
@@ -93,7 +182,8 @@ async def play(_, message: Message):
                         ]
                     ]
                 )
-            
+        requested_by = message.from_user.mention()
+        await generate_cover(requested_by, title, views, duration, thumbnail)     
         file_path = await converter.convert(youtube.download(url))
     else:
         await lel.edit("🔎 **Finding** the song...")
@@ -139,21 +229,26 @@ async def play(_, message: Message):
                     ]
                 ]
             )
+        requested_by = message.from_user.mention()
+        await generate_cover(requested_by, title, views, duration, thumbnail)  
         file_path = await converter.convert(youtube.download(url))
-        
+  
     if message.chat.id in callsmusic.pytgcalls.active_calls:
         position = await queues.put(message.chat.id, file=file_path)
         await message.reply_photo(
-        photo=thumb_name, 
+        photo="final.png", 
         caption=f"#⃣ Your requested song **queued** at position {position}!",
         reply_markup=keyboard)
+        os.remove("final.png")
         return await lel.delete()
     else:
         callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
         await message.reply_photo(
-        photo=thumb_name,
+        photo="final.png",
         reply_markup=keyboard,
         caption="▶️ **Playing** here the song requested by {} via DaisyX Music 😜".format(
         message.from_user.mention()
         ),
     )
+        os.remove("final.png")
+        return await lel.delete()
