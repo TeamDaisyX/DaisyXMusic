203d3f68014b088813433a8d8512c0cdd49c22df
Inuka Asith <inukaasith7@gmail.com>
2021-04-23 20:12:46 +0530

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -34,11 +34,11 @@ import pyrogram
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-@app.on_message(pyrogram.filters.command(["search"]))
+@app.on_message(pyrogram.filters.command(["yts"]))
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
+            await message.reply_text("/yts needs an argument!")
             return
         query = message.text.split(None, 1)[1]
         m = await message.reply_text("Searching....")
