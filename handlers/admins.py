be7c95bfa0df1839b8b14be91b940515e48b6e66
kucekmill <79855424+irwanramadhan@users.noreply.github.com>
2021-04-30 21:41:00 +0700

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -31,7 +31,7 @@ from helpers.filters import command, other_filters
 from helpers.decorators import errors, authorized_users_only
 from config import que, admins as a
 
-@Client.on_message(filters.command('adminreset'))
+@Client.on_message(filters.command("adminreset"))
 async def update_admin(client, message):
     global a
     admins = await client.get_chat_members(message.chat.id, filter="administrators")
@@ -53,7 +53,7 @@ async def pause(_, message: Message):
     ) or (
             callsmusic.pytgcalls.active_calls[message.chat.id] == 'paused'
     ):
-        await message.reply_text("❗ Tidak ada Lagu yang sedang diputar!")
+        await message.reply_text("❗Tidak ada Lagu yang sedang diputar!")
     else:
         callsmusic.pytgcalls.pause_stream(message.chat.id)
         await message.reply_text("▶️ Paused!")
