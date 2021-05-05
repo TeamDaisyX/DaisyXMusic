cc6157bb04285d8a4193856d46ed197ec06aa6f8
kucekmill <79855424+kucekmill@users.noreply.github.com>
2021-05-06 03:03:55 +0700

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -31,7 +31,7 @@ from helpers.filters import command, other_filters
 from helpers.decorators import errors, authorized_users_only
 from config import que, admins as a
 
-@Client.on_message(filters.command('adminreset'))
+@Client.on_message(filters.command('admincache'))
 async def update_admin(client, message):
     global a
     admins = await client.get_chat_members(message.chat.id, filter="administrators")
@@ -118,9 +118,9 @@ async def skip(_, message: Message):
 
 
 @Client.on_message(
-    filters.command("admincache")
+    filters.command("adminreset")
 )
 @errors
 async def admincache(client, message: Message):
     set(message.chat.id, [member.user for member in await message.chat.get_members(filter="administrators")])
-    #await message.reply_text('Admin cache refreshed✅ !')
+    #await message.reply_text( 'Admin cache refreshed!')
