7a6d5225e1a9b5a2ef00736d48962e5972512670
Inuka Asith <inukaasith7@gmail.com>
2021-05-14 16:52:26 +0530

    Update __init__.py

--- DaisyXMusic/function/__init__.py
+++ DaisyXMusic/function/__init__.py
@@ -1,3 +1,4 @@
-from DaisyXMusic.helpers.admins import admins, get, set
+from DaisyXMusic.helpers.admins import get, set
+from DaisyXMusic.function.admins import admins
 
 __all__ = ["set", "get", "admins"]
