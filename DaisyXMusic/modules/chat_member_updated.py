a15fdb29bc6362bdd5be6e14c833ea245a45b311
InukaAsith <inukaasith7@gmail.com>
2021-05-14 10:36:22 +0000

    Auto Fixes: Code formatted

--- DaisyXMusic/modules/chat_member_updated.py
+++ DaisyXMusic/modules/chat_member_updated.py
@@ -6,26 +6,22 @@ from DaisyXMusic.function import *
 
 @Client.on_chat_member_updated()
 async def chat_member_updated(_, chat_member_updated: ChatMemberUpdated):
-    if chat_member_updated.new_chat_member \
-            and chat_member_updated.old_chat_member:
+    if chat_member_updated.new_chat_member and chat_member_updated.old_chat_member:
         (
             admins.admins[chat_member_updated.chat.id].append(
                 chat_member_updated.new_chat_member.user.id,
             )
         ) if (
-            (
-                chat_member_updated.new_chat_member.can_manage_voice_chats
-            ) and (
-                (
-                    chat_member_updated.new_chat_member.user.id
-                ) not in admins.admins[chat_member_updated.chat.id]
+            (chat_member_updated.new_chat_member.can_manage_voice_chats)
+            and (
+                (chat_member_updated.new_chat_member.user.id)
+                not in admins.admins[chat_member_updated.chat.id]
             )
         ) else (
             admins.admins[chat_member_updated.chat.id].remove(
                 chat_member_updated.new_chat_member.user.id,
             )
         ) if (
-            (
-                chat_member_updated.new_chat_member.user.id
-            ) in admins.admins[chat_member_updated.chat.id]
+            (chat_member_updated.new_chat_member.user.id)
+            in admins.admins[chat_member_updated.chat.id]
         ) else None
