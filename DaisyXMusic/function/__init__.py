3b52c427c5e6f744e6e93336f97fafd481b3a75a
InukaAsith <inukaasith7@gmail.com>
2021-05-14 11:22:51 +0000

    Auto Fixes: Code formatted

--- DaisyXMusic/function/__init__.py
+++ DaisyXMusic/function/__init__.py
@@ -1,4 +1,4 @@
-from DaisyXMusic.helpers.admins import get, set
 from DaisyXMusic.function.admins import admins
+from DaisyXMusic.helpers.admins import get, set
 
 __all__ = ["set", "get", "admins"]
