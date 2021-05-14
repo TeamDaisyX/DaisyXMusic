c0d32db82f5f5353ca21e3ba782125c9a8ad5082
Inuka Asith <inukaasith7@gmail.com>
2021-05-14 16:06:02 +0530

    Update chat_member_updated.py

--- DaisyXMusic/modules/chat_member_updated.py
+++ DaisyXMusic/modules/chat_member_updated.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from pyrogram.types import ChatMemberUpdated
 
-from DaisyXmusic.function import *
+from DaisyXMusic.function import *
 
 
 @Client.on_chat_member_updated()
