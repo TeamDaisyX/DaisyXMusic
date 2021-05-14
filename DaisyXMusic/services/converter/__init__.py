3bf30f2e2c584bcf39db186a454088863145d205
Inuka Asith <inukaasith7@gmail.com>
2021-05-14 18:11:23 +0530

    Update __init__.py

--- DaisyXMusic/services/converter/__init__.py
+++ DaisyXMusic/services/converter/__init__.py
@@ -3,4 +3,4 @@ from os import listdir, mkdir
 if "raw_files" not in listdir():
     mkdir("raw_files")
 
-from DaisyXMusic.services.converter import convert
+from DaisyXMusic.services.converter.converter import convert
