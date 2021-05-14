9557ba5717afc623eb768c14a31b1d9a47f63cf8
InukaAsith <inukaasith7@gmail.com>
2021-05-14 11:57:52 +0000

    Auto Fixes: Code formatted

--- DaisyXMusic/modules/admins.py
+++ DaisyXMusic/modules/admins.py
@@ -21,10 +21,10 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from DaisyXMusic.function.admins import set
+from DaisyXMusic.helpers.channelmusic import get_chat_id
 from DaisyXMusic.helpers.decorators import authorized_users_only, errors
 from DaisyXMusic.helpers.filters import command, other_filters
 from DaisyXMusic.services.callsmusic import callsmusic
-from DaisyXMusic.helpers.channelmusic import get_chat_id
 
 
 @Client.on_message(filters.command("adminreset"))
