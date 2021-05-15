afc5ec6eef169bba86f9a66b1b4245d4d4e1a434
Inuka Asith <inukaasith7@gmail.com>
2021-05-15 09:47:44 +0530

    Update channelmusic.py

--- DaisyXMusic/helpers/channelmusic.py
+++ DaisyXMusic/helpers/channelmusic.py
@@ -3,5 +3,5 @@ from pyrogram.types import Chat
 
 def get_chat_id(chat: Chat):
     if chat.title.startswith("Channel Music: ") and chat.title[16:].isnumeric():
-        return int(chat.title[13:])
+        return int(chat.title[15:])
     return chat.id
