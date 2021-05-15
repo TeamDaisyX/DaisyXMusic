a5c0b4ff812d429d07a4b4a99dbe7d4b24d45e37
Inuka Asith <inukaasith7@gmail.com>
2021-05-15 14:03:37 +0530

    Update config.py

--- DaisyXMusic/config.py
+++ DaisyXMusic/config.py
@@ -34,7 +34,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
-
+ARQ_API_KEY = getenv("ARQ_API_KEY","VKSSQH-SRNJYZ-MSOJAI-QALESR-ARQ")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
