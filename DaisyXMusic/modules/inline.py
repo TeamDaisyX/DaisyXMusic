e2942812abbef55ef87fb92aafa6b05bb97443ac
rilking33 <84245593+rilking33@users.noreply.github.com>
2021-05-15 18:50:22 +0000

    inline.py

--- DaisyXMusic/modules/inline.py
+++ DaisyXMusic/modules/inline.py
@@ -31,7 +31,7 @@ async def inline(client: Client, query: InlineQuery):
                         result["duration"], result["viewCount"]["short"]
                     ),
                     input_message_content=InputTextMessageContent(
-                        "https://www.youtube.com/watch?v={}".format(result["id"])
+                        "/play https://www.youtube.com/watch?v={}".format(result["id"])
                     ),
                     thumb_url=result["thumbnails"][0]["url"],
                 )
