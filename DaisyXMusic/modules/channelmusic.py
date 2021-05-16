b2be7c1c83fec1b0ecd0403a9f74c6216cb6cfad
Inuka Asith <inukaasith7@gmail.com>
2021-05-16 13:29:38 +0530

    Update channelmusic.py

--- DaisyXMusic/modules/channelmusic.py
+++ DaisyXMusic/modules/channelmusic.py
@@ -354,7 +354,7 @@ async def m_cb(b, cb):
             await cb.answer("Chat is not connected!", show_alert=True)
 
 
-@Client.on_message(filters.command(["channelplay","cplay"]) & other_filters)
+@Client.on_message(filters.command(["channelplay","cplay"])  & filters.group & ~filters.edited)
 @authorized_users_only
 async def play(_, message: Message):
     global que
