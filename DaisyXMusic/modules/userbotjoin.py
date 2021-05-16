68e595003cf77c4ff68b5ab27415965d65fa4d90
Inuka Asith <inukaasith7@gmail.com>
2021-05-16 11:53:40 +0530

    Update userbotjoin.py

--- DaisyXMusic/modules/userbotjoin.py
+++ DaisyXMusic/modules/userbotjoin.py
@@ -69,3 +69,47 @@ async def rem(USER, message):
             "\n\nOr manually kick me from to your Group</b>",
         )
         return
+
+@Client.on_message(filters.command(["userbotjoinchannel"]) & ~filters.private & ~filters.bot)
+@authorized_users_only
+@errors
+async def addcchannel(client, message):
+    try:
+      conchat = await client.get_chat(message.chat.id)
+      conid = conchat.linked_chat.id
+      chid = conid
+    except:
+      await message.reply("Is chat even linked")
+      return    
+    chat_id = chid
+    try:
+        invitelink = await client.export_chat_invite_link(chid)
+    except:
+        await message.reply_text(
+            "<b>Add me as admin of yor group first</b>",
+        )
+        return
+
+    try:
+        user = await USER.get_me()
+    except:
+        user.first_name = "DaisyMusic"
+
+    try:
+        await USER.join_chat(invitelink)
+        await USER.send_message(message.chat.id, "I joined here as you requested")
+    except UserAlreadyParticipant:
+        await message.reply_text(
+            "<b>helper already in your chat</b>",
+        )
+    except Exception as e:
+        print(e)
+        await message.reply_text(
+            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
+            "\n\nOr manually add @DaisyXhelper to your Group and try again</b>",
+        )
+        return
+    await message.reply_text(
+        "<b>helper userbot joined your chat</b>",
+    )
+    
