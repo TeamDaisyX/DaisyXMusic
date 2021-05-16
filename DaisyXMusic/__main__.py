ff87b05a21df25a8fb0fc4af98befbe06d206c72
Inuka Asith <inukaasith7@gmail.com>
2021-05-16 15:53:29 +0530

    Update __main__.py

--- DaisyXMusic/__main__.py
+++ DaisyXMusic/__main__.py
@@ -1,5 +1,5 @@
-# Calls Music 1 - Telegram bot for streaming audio in group calls
-# Copyright (C) 2021  Roj Serbest
+# DaisyXMusic (Telegram bot project)
+# Copyright (C) 2021  Inuka Asith & Rojserbest
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
