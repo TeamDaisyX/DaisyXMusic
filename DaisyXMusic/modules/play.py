676c6e74761d44b55c7cbd7212407190e91502a7
Inuka Asith <inukaasith7@gmail.com>
2021-05-23 10:26:06 +0530

    😎

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -1,6 +1,6 @@
 # Daisyxmusic (Telegram bot project)
 # Copyright (C) 2021  Inukaasith
-
+# Copyright (C) 2021  TheHamkerCat (Python_ARQ)
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
