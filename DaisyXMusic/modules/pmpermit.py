dc2d1c3c8d957a654ddc83f4d4070d702bd78ace
Inuka Asith <inukaasith7@gmail.com>
2021-05-28 08:18:23 +0530

    .a .da and approovals

--- DaisyXMusic/modules/pmpermit.py
+++ DaisyXMusic/modules/pmpermit.py
@@ -22,12 +22,15 @@ from pyrogram.types import Message
 from DaisyXMusic.services.callsmusic.callsmusic import client as USER
 
 PMSET =True
-
+pchats = []
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
     if PMPERMIT == "ENABLE":
         if PMSET:
+            chat_id = message.chat.id
+            if chat_id in pchats:
+                return
             await USER.send_message(
                 message.chat.id,
                 "Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n",
@@ -50,3 +53,31 @@ async def bye(client: Client, message: Message):
             PMSET = None
             await message.reply_text("Pmpermit turned off")
             return
+
+@USER.on_message(filters.text & filters.private & filters.me)        
+async def autopmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if not chat_id in pchats:
+        pchats.append(chat_id)
+        await message.reply_text("Approoved to PM due to outgoing messages")
+        return
+    message.continue_propagation()    
+    
+@USER.on_message(filters.command("a", [".", ""]) & filters.me & filters.private)
+async def pmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if not chat_id in pchats:
+        pchats.append(chat_id)
+        await message.reply_text("Approoved to PM")
+        return
+    message.continue_propagation()    
+    
+
+@USER.on_message(filters.command("da", [".", ""]) & filters.me & filters.private)
+async def rmpmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if chat_id in pchats:
+        pchats.remove(chat_id)
+        await message.reply_text("Dispprooved to PM")
+        return
+    message.continue_propagation()    
