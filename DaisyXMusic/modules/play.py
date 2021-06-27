954766e8695ce2f700d22caa8d02e7133f7ceb6a
Inuka Asith <inukaasith7@gmail.com>
2021-06-27 05:45:57 +0530

    Update play.py

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -233,7 +233,7 @@ async def settings(client, message):
     stats = updated_stats(message.chat, queue)
     if stats:
         if playing:
-            await message.reply(stats, reply_markup=r_ply("pause") 
+            await message.reply(stats, reply_markup=r_ply("pause")) 
         else:
             await message.reply(stats, reply_markup=r_ply("play"))
     else:
