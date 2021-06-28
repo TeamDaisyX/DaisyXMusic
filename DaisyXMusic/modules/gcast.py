79fd95cd3e13d5d468f42196049132e3ecade88f
ryostar <82922436+ryostar@users.noreply.github.com>
2021-06-28 16:02:14 +0700

    Update gcast.py

--- DaisyXMusic/modules/gcast.py
+++ DaisyXMusic/modules/gcast.py
@@ -15,31 +15,36 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from pyrogram import Client
-from pyrogram import filters
-from pyrogram.errors import UserAlreadyParticipant
 import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.types import Dialog, Chat, Message
+from pyrogram.errors import UserAlreadyParticipant
+
+from DaisyXMusic.services.callsmusic.callsmusic import client as pakaya
 from DaisyXMusic.config import SUDO_USERS
 
-@Client.on_message(filters.command(["gcast"]))
-async def bye(client, message):
+@Client.on_message(filters.command(["broadcast"]))
+async def broadcast(_, message: Message):
     sent=0
     failed=0
-    if message.from_user.id in SUDO_USERS:
-        lol = await message.reply("Starting Gcast")
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        wtf = await message.reply("`Starting a broadcast...`")
         if not message.reply_to_message:
-            await lol.edit("Reply to any text message to gcast sir")
+            await wtf.edit("Please Reply to a Message to broadcast!")
             return
-        msg = message.reply_to_message.text
-        async for dialog in client.iter_dialogs():
+        lmao = message.reply_to_message.text
+        async for dialog in pakaya.iter_dialogs():
             try:
-                await client.send_message(dialog.chat.id, msg)
+                await pakaya.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
+                await wtf.edit(f"`broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
-                await asyncio.sleep(0.7)
+                #await wtf.edit(f"`broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
                 
-        await message.reply_text(f"Gcasted message to {sent} chats. Failed {failed} chats.")
+            
+        await message.reply_text(f"`Broadcast Finished ` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
