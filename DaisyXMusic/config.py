3b4a30fdad4bad3b534fcfd3954f1e0cb0bebf94
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-06-30 10:14:39 +0530

    Update config.py

--- DaisyXMusic/config.py
+++ DaisyXMusic/config.py
@@ -15,8 +15,9 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # Modified by Inukaasith
 
-from os import getenv
 import os
+from os import path
+from os import getenv
 from dotenv import load_dotenv
 
 if os.path.exists("local.env"):
