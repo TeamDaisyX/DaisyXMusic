c8de845e02279b5c4fc2976e554c053f36946666
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-07-02 20:01:57 +0530

    Update admins.py

--- DaisyXMusic/function/admins.py
+++ DaisyXMusic/function/admins.py
@@ -1,6 +1,6 @@
 from typing import Dict
 from typing import List
-from typing import Union
+
 
 admins: Dict[int, List[int]] = {}
 
@@ -9,8 +9,7 @@ def set(chat_id: int, admins_: List[int]):
     admins[chat_id] = admins_
 
 
-def get(chat_id: int) -> Union[List[int], bool]:
+def get(chat_id: int) -> List[int]:
     if chat_id in admins:
         return admins[chat_id]
-
-    return False
+    return []
