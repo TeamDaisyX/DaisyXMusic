e5b3f19326c1d395c4af2848cc413c2027bd05b6
Myting0508 <86727370+Myting0508@users.noreply.github.com>
2021-07-03 09:44:28 +0700

    Update msg.py

--- DaisyXMusic/modules/msg.py
+++ DaisyXMusic/modules/msg.py
@@ -33,99 +33,80 @@ f"""
 """,
 
 f"""
-**Setting up**
-
-1) Make bot admin (Group and in channel if use cplay)
-2) Start a voice chat
-3) Try /play [song name] for the first time by an admin
-*) If userbot joined enjoy music, If not add @{ASSISTANT_NAME} to your group and retry
-
-**For Channel Music Play**
-1) Make me admin of your channel 
-2) Send /userbotjoinchannel in linked group
-3) Now send commands in linked group
+**Pengaturan Utama**
+1) Membuat bot admin (Group dan di channel jika menggunakan cplay)
+2) Mulai obrolan suara
+3) Coba /play [nama lagu] pertama kali oleh admin
+*) Jika userbot bergabung nikmati musik, Jika tidak tambahkan @{ASSISTANT_NAME} ke grup Anda dan coba lagi
+**Untuk Channel Music Play**
+1) Jadikan saya admin saluran Anda
+2) Kirim /userbotjoinchannel di grup tertaut
+3) Sekarang kirim perintah di grup tertaut
+**Beberapa Command**
+**◎› Memainkan Lagu 🎧**
+• /play <nama lagu> : putar lagu yang Anda minta
+• /play <url youtube> : Putar lagu melalui balasan url youtube
+• /play <balas ke audio> : putar file balasan
+• /dplay <nama lagu> : putar lagu yang Anda minta melalui deezer
+• /splay <nama lagu> : putar lagu yang Anda minta melalui jio saavn
+**◎› Playback ⏯**
+• /player: buka panel pengaturan pemutar musik
+• /skip: putar lagu berikutnya
+• /pause: jeda pemutaran lagu
+• /resume: melanjutkan pemutaran lagu
+• /end: hentikan pemutaran musik
+• /current: Tampilkan sedang diputar
+• /playlist: Tampilkan daftar yang sedang diputar
+__**Cmd player dan semua cmd lain kecuali**__ /play, /current __**dan**__ /playlist __**hanya untuk admin grup**__
 """,
+        
 f"""
-**Commands**
-
-**=>> Song Playing 🎧**
-
-- /play: Play the requestd song
-- /play [yt url] : Play the given yt url
-- /play [reply yo audio]: Play replied audio
-- /dplay: Play song via deezer
-- /splay: Play song via jio saavn
-- /ytplay: Directly play song via Youtube Music
-
-**=>> Playback ⏯**
-
-- /player: Open Settings menu of player
-- /skip: Skips the current track
-- /pause: Pause track
-- /resume: Resumes the paused track
-- /end: Stops media playback
-- /current: Shows the current Playing track
-- /playlist: Shows playlist
-
-*Player cmd and all other cmds except /play, /current  and /playlist  are only for admins of the group.
-""",
-
-f"""
-**=>> Channel Music Play 🛠**
-
-⚪️ For linked group admins only:
-
-- /cplay [song name] - play song you requested
-- /cdplay [song name] - play song you requested via deezer
-- /csplay [song name] - play song you requested via jio saavn
-- /cplaylist - Show now playing list
-- /cccurrent - Show now playing
-- /cplayer - open music player settings panel
-- /cpause - pause song play
-- /cresume - resume song play
-- /cskip - play next song
-- /cend - stop music play
-- /userbotjoinchannel - invite assistant to your chat
-
-channel is also can be used instead of c ( /cplay = /channelplay )
-
-⚪️ If you donlt like to play in linked group:
-
-1) Get your channel ID.
-2) Create a group with tittle: Channel Music: your_channel_id
-3) Add bot as Channel admin with full perms
-4) Add @{ASSISTANT_NAME} to the channel as an admin.
-5) Simply send commands in your group. (remember to use /ytplay instead /play)
+**◎› Putar Musik Di Channel 📮**
+⚪️ Hanya untuk admin grup tertaut:
+• /cplay [song name] - putar lagu yang Anda minta
+• /cdplay [song name] - putar lagu yang Anda minta via deezer
+• /csplay [song name] - putar lagu yang Anda minta via jio saavn
+• /cplaylist - Perlihatkan daftar yang dimainkan
+• /cccurrent - Perlihatkan yang diputar sekarang
+• /cplayer - buka panel pengaturan pemutar musik
+• /cpause - jeda pemutaran lagu
+• /cresume - lanjutkan pemutaran lagu
+• /cskip - putar lagu berikutnya
+• /cend - stop pemutaran lagu
+• /userbotjoinchannel - Undang asisten ke chat kamu
+saluran Channel juga dapat digunakan sebagai pengganti c ( /cplay = /channelplay )
+⚪️ Jika Anda tidak suka bermain di grup tertaut:
+1) Dapatkan ID saluran Anda.
+2) Buat grup dengan judul: Channel Music: your_channel_id
+3) Tambahkan bot sebagai admin Saluran dengan izin penuh
+4) Tambahkan @{ASSISTANT_NAME} ke saluran sebagai admin.
+5) Cukup kirim perintah di grup Anda.
 """,
 
 f"""
-**=>> More tools 🧑‍🔧**
-
-- /musicplayer [on/off]: Enable/Disable Music player
-- /admincache: Updates admin info of your group. Try if bot isn't recognize admin
-- /userbotjoin: Invite @{ASSISTANT_NAME} Userbot to your chat
-""",
-f"""
-**=>> Song Download 🎸**
-
-- /video [song mame]: Download video song from youtube
-- /song [song name]: Download audio song from youtube
-- /saavn [song name]: Download song from saavn
-- /deezer [song name]: Download song from deezer
-
-**=>> Search Tools 📄**
-
-- /search [song name]: Search youtube for songs
-- /lyrics [song name]: Get song lyrics
-""",
-
-f"""
-**=>> Commands for Sudo Users ⚔️**
-
- - /userbotleaveall - remove assistant from all chats
- - /broadcast <reply to message> - globally brodcast replied message to all chats
- - /pmpermit [on/off] - enable/disable pmpermit message
-*Sudo Users can execute any command in any groups
-
+**◎› More Info 📲**
+• /admincache: Memperbarui info admin grup Anda. Coba jika bot tidak mengenali admin
+• /userbotjoin: Undang @{ASSISTANT_NAME} Userbot ke obrolan Anda
+**◎› 📝 Command Khusus buat pengguna sudo**
+ • /userbotleaveall - Keluarkan asisten musik dari semua obrolan chat
+ • /broadcast <balas ke pesan> - global brodcast membalas pesan ke semua obrolan
+ • /pmpermit [on/off] - enable/disable pesan pmpermit 
+__Pengguna Sudo dapat menjalankan perintah apa pun di grup mana pun__
+◎› Owner Project: [Yunus Zend](https://t.me/ZendYNS)
+◎› Source Code: [Click Here](https://github.com/Yunus-ZEND/VC-MusicINDO)
 """
       ]
+
+# Editor by Creator And Contributor
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
