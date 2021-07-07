7a42f0c81af52602846292e924cb5dedd5e05bd0
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-07-07 08:37:17 +0530

    Bug Fixes

--- DaisyXMusic/services/queues/queues.py
+++ DaisyXMusic/services/queues/queues.py
@@ -1,7 +1,14 @@
-from asyncio import Queue
+from asyncio import Queue as _Queue
 from asyncio import QueueEmpty as Empty
-from typing import Dict 
-from typing import Union
+from typing import Dict
+
+
+class Queue(_Queue):
+    _queue: list = []
+
+    def clear(self):
+        self._queue.clear()
+
 
 queues: Dict[int, Queue] = {}
 
@@ -13,12 +20,13 @@ async def put(chat_id: int, **kwargs) -> int:
     return queues[chat_id].qsize()
 
 
-def get(chat_id: int) -> Union[Dict[str, str], None]:
+def get(chat_id: int) -> Dict[str, str]:
     if chat_id in queues:
         try:
             return queues[chat_id].get_nowait()
         except Empty:
-            return None
+            return {}
+    return {}
 
 
 def is_empty(chat_id: int) -> bool:
@@ -40,5 +48,5 @@ def clear(chat_id: int):
         if queues[chat_id].empty():
             raise Empty
         else:
-            queues[chat_id].queue = []
+            queues[chat_id].clear()
     raise Empty
