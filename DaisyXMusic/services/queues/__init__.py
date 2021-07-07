f98649804dec27043e39dd7106e0f615dcd300fa
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-07-07 08:44:17 +0530

    Update __init__.py

--- DaisyXMusic/services/queues/__init__.py
+++ DaisyXMusic/services/queues/__init__.py
@@ -1,7 +1,3 @@
-from DaisyXMusic.services.queues.queues import (
-    clear,
-    get,
-    is_empty,
-    put,
-    task_done,
-)
+from DaisyXMusic.services.queues.queues import clear, get, is_empty, put, task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
