087d55308a88801210905b6dce312cca04bbda1d
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-07-08 10:17:32 +0530

    Lol

--- DaisyXMusic/services/downloaders/youtube.py
+++ DaisyXMusic/services/downloaders/youtube.py
@@ -39,12 +39,8 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
-        )
-    try:
-        ydl.download([url])
-    except:
-        raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
-        )
+            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
+            f"the provided video is {duration} minute(s)",
+       )
+    ydl.download([url])
     return path.join("downloads", f"{info['id']}.{info['ext']}")
