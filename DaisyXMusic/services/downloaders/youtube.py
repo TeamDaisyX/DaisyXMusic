55197e91e518fd4e733a09f25c57d532247ce2ea
Aniket-Code10010 <86464839+Aniket-Code10010@users.noreply.github.com>
2021-07-12 22:30:05 +0530

    Update youtube.py

--- DaisyXMusic/services/downloaders/youtube.py
+++ DaisyXMusic/services/downloaders/youtube.py
@@ -19,8 +19,8 @@ from os import path
 
 from youtube_dl import YoutubeDL
 
-from VCPlayBot.config import DURATION_LIMIT
-from VCPlayBot.helpers.errors import DurationLimitError
+from DaisyXMusic.config import DURATION_LIMIT
+from DaisyXMusic.helpers.errors import DurationLimitError
 
 ydl_opts = {
     "format": "bestaudio[ext=m4a]",
