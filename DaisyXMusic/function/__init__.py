8992d8e2db4efc3afaac7a90c29185aba55fde95
matheesha official <77784129+PercyOfficial@users.noreply.github.com>
2021-07-19 10:39:37 +0530

    Delete DaisyXMusic directory

--- DaisyXMusic/function/__init__.py
+++
@@ -1,5 +0,0 @@
-from DaisyXMusic.function.admins import admins
-from DaisyXMusic.function.admins import get
-from DaisyXMusic.function.admins import set
-
-__all__ = ["set", "get", "admins"]
--- DaisyXMusic/services/downloaders/__init__.py
+++
@@ -1,3 +0,0 @@
-from DaisyXMusic.services.downloaders import youtube
-
-__all__ = ["youtube"]
