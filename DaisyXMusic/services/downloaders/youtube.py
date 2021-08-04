19b4351a45af5119c0d59d90183857d270575026
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-08-04 12:08:05 +0530

    Add verbose

--- DaisyXMusic/services/downloaders/youtube.py
+++ DaisyXMusic/services/downloaders/youtube.py
@@ -25,7 +25,8 @@ from DaisyXMusic.config import DURATION_LIMIT
 from DaisyXMusic.helpers.errors import DurationLimitError
 
 ydl_opts = {
-    "format": "bestaudio[ext=m4a]",
+    "format": "bestaudio/best",
+    "verbose": True,
     "geo-bypass": True,
     "nocheckcertificate": True,
     "outtmpl": "downloads/%(id)s.%(ext)s",
