55efa40d8b92303040dbdd5e31d1bd95a6930583
Inuka Asith <inukaasith7@gmail.com>
2021-08-13 18:08:31 +0530

    Update channeladmins.py

--- DaisyXMusic/modules/channeladmins.py
+++ DaisyXMusic/modules/channeladmins.py
@@ -1,5 +1,7 @@
 # Calls Music 1 - Telegram bot for streaming audio in group calls
 # Copyright (C) 2021  Roj Serbest
+# Copyright (C) 2021  InukaAsith & Technical-Hunter (Optimized for channel play)
+
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
