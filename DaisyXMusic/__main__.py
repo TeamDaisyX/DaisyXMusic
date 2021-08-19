f2cf643c9fcc6a2cb48cb17037947d87ad812c7e
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-08-19 19:13:50 +0530

    Lol

--- DaisyXMusic/__main__.py
+++ DaisyXMusic/__main__.py
@@ -1,4 +1,5 @@
 # DaisyXMusic (Telegram bot project)
+
 # Copyright (C) 2021  Inuka Asith & Rojserbest
 
 # This program is free software: you can redistribute it and/or modify
@@ -10,7 +11,7 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-#
+
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
