9f23c1ba4b5910009241ae246a0fa8906a44f1be
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-08-23 21:07:49 +0530

    Sad

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -337,9 +337,7 @@ async def m_cb(b, cb):
 
     the_data = cb.message.reply_markup.inline_keyboard[1][0].callback_data
     if type_ == "pause":
-        if (chet_id in callsmusic.pytgcalls.active_calls) or (
-            not in callsmusic.pytgcalls.active_calls[chet_id] == "paused"
-        ):
+        if chet_id in callsmusic.pytgcalls.active_calls:
             await cb.answer("Chat is not connected!", show_alert=True)
         else:
             await callsmusic.pytgcalls.pause_stream(chet_id)
@@ -349,9 +347,7 @@ async def m_cb(b, cb):
             )
 
     elif type_ == "play":
-        if (chet_id in callsmusic.pytgcalls.active_calls) or (
-            not in callsmusic.pytgcalls.active_calls[chet_id] == "playing"
-        ):
+        if chet_id in callsmusic.pytgcalls.active_calls:
             await cb.answer("Chat is not connected!", show_alert=True)
         else:
             await callsmusic.pytgcalls.resume_stream(chet_id)
@@ -384,17 +380,13 @@ async def m_cb(b, cb):
         await cb.message.edit(msg)
 
     elif type_ == "resume":
-        if (chet_id in callsmusic.pytgcalls.active_calls) or (
-            not in callsmusic.pytgcalls.active_calls[chet_id] == "playing"
-        ):
+        if chet_id in callsmusic.pytgcalls.active_calls:
             await cb.answer("Chat is not connected or already playng", show_alert=True)
         else:
             await callsmusic.pytgcalls.resume_stream(chet_id)
             await cb.answer("Music Resumed!")
     elif type_ == "puse":
-        if (chet_id in callsmusic.pytgcalls.active_calls) or (
-            not in callsmusic.pytgcalls.active_calls[chet_id] == "paused"
-        ):
+        if chet_id in callsmusic.pytgcalls.active_calls:
             await cb.answer("Chat is not connected or already paused", show_alert=True)
         else:
             await callsmusic.pytgcalls.pause_stream(chet_id)
