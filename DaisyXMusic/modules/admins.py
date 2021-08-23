6edd9853aad99763b8e455b278466594f3991a0b
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-08-23 20:15:57 +0530

    Okay

--- DaisyXMusic/modules/admins.py
+++ DaisyXMusic/modules/admins.py
@@ -80,9 +80,7 @@ async def deautenticate(client, message):
 @authorized_users_only
 async def pause(_, message: Message):
     chat_id = get_chat_id(message.chat)
-    if (chat_id in callsmusic.pytgcalls.active_calls) or (
-        not in callsmusic.pytgcalls.active_calls[chat_id] == "paused"
-    ):
+    if chat_id in callsmusic.pytgcalls.active_calls:
         await message.reply_text("❗ Nothing is playing!")
     else:
         await callsmusic.pytgcalls.pause_stream(chat_id)
@@ -94,9 +92,7 @@ async def pause(_, message: Message):
 @authorized_users_only
 async def resume(_, message: Message):
     chat_id = get_chat_id(message.chat)
-    if (chat_id in callsmusic.pytgcalls.active_calls) or (
-        not in callsmusic.pytgcalls.active_calls[chat_id] == "playing"
-    ):
+    if chat_id in callsmusic.pytgcalls.active_calls:
         await message.reply_text("❗ Nothing is paused!")
     else:
         await callsmusic.pytgcalls.resume_stream(chat_id)
