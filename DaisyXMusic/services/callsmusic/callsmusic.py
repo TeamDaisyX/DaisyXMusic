fb71f19f0f636cd73b130d389e232e17a15185d6
Hunter XDD <85411336+Hunter-XDD@users.noreply.github.com>
2021-08-31 23:36:30 +0530

    nothing

--- DaisyXMusic/services/callsmusic/callsmusic.py
+++ DaisyXMusic/services/callsmusic/callsmusic.py
@@ -28,7 +28,7 @@ active_chats: Dict[int, Dict[str, bool]] = {}
 
 def init_instance(chat_id: int):
     if chat_id not in instances:
-        instances[chat_id] = GroupCallFactory(client,outgoing_audio_bitrate_kbit=512).get_file_group_call()
+        instances[chat_id] = GroupCallFactory(client,outgoing_audio_bitrate_kbit=320).get_file_group_call()
 
     instance = instances[chat_id]
 
