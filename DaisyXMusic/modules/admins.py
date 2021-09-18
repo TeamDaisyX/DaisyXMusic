b017838f55fde9f1716bbf42d7d88ef2596fe1c4
Hunter XDD <85411336+Hunter-XDD@users.noreply.github.com>
2021-09-18 13:06:49 +0530

    fix some bugs and add mute and unmute

--- DaisyXMusic/modules/admins.py
+++ DaisyXMusic/modules/admins.py
@@ -20,13 +20,13 @@ from asyncio import QueueEmpty
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from DaisyXMusic.config import que
 from DaisyXMusic.function.admins import set
 from DaisyXMusic.helpers.channelmusic import get_chat_id
 from DaisyXMusic.helpers.decorators import authorized_users_only, errors
 from DaisyXMusic.helpers.filters import command, other_filters
 from DaisyXMusic.services.callsmusic import callsmusic
 from DaisyXMusic.services.queues import queues
+from DaisyXMusic.config import que
 
 
 @Client.on_message(filters.command("adminreset"))
@@ -47,13 +47,12 @@ async def update_admin(client, message: Message):
 @authorized_users_only
 async def pause(_, message: Message):
     chat_id = get_chat_id(message.chat)
-    if (chat_id not in callsmusic.active_chats) or (
-        callsmusic.active_chats[chat_id] == "paused"
-    ):
-        await message.reply_text("❗ Nothing is playing!")
-    else:
-        callsmusic.pause(chat_id)
         await message.reply_text("▶️ Paused!")
+      if:
+        callsmusic.pause(chat_id)
+      else:
+        await message.reply_text("❗ Nothing is playing!")
+        
 
 
 @Client.on_message(command("resume") & other_filters)
@@ -61,13 +60,12 @@ async def pause(_, message: Message):
 @authorized_users_only
 async def resume(_, message: Message):
     chat_id = get_chat_id(message.chat)
-    if (chat_id not in callsmusic.active_chats) or (
-        callsmusic.active_chats[chat_id] == "playing"
-    ):
-        await message.reply_text("❗ Nothing is paused!")
-    else:
-        callsmusic.resume(chat_id)
         await message.reply_text("⏸ Resumed!")
+      if:
+        callsmusic.resume(chat_id)
+      else:
+        await message.reply_text("❗ Nothing is paused!")
+        
 
 
 @Client.on_message(command("end") & other_filters)
@@ -108,6 +106,40 @@ async def skip(_, message: Message):
     if not qeue:
         return
     await message.reply_text(f"- Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**")
+    
+
+@Client.on_message(command('mute') & other_filters)
+@errors
+@authorized_users_only
+async def mute(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    result = callsmusic.mute(chat_id)
+        await message.reply_text("✅ Muted")
+      if:
+        result == 0
+      else:
+        await message.reply_text("❌ Already muted")
+      if:
+        result == 1
+      else:
+        await message.reply_text("❌ Not in call")
+
+        
+@Client.on_message(command('unmute') & other_filters)
+@errors
+@authorized_users_only
+async def unmute(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    result = callsmusic.unmute(chat_id)
+        await message.reply_text("✅ Unmuted")
+      if:
+        result == 0
+      else:
+        await message.reply_text("❌ Not muted")
+      if:
+        result == 1
+      else:
+        await message.reply_text("❌ Not in call")
 
 
 @Client.on_message(filters.command("admincache"))
