720cd0e697c6b6a26e8a34e9e9c0dc8573da8feb
Attitude king Vijay <90513215+Vijay111135A@users.noreply.github.com>
2021-10-01 23:12:59 +0530

    Update msg.py

--- DaisyXMusic/modules/msg.py
+++ DaisyXMusic/modules/msg.py
@@ -18,14 +18,14 @@ from DaisyXMusic.config import ASSISTANT_NAME, PROJECT_NAME
 
 
 class Messages:
-    START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info."
+    START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels Made by @call_me_Vijay1 .\n\n✅ Send me /help for more info."
     HELP_MSG = [
         ".",
         f"""
 **Hey 👋 Welcome back to {PROJECT_NAME}
 
-⚪️ {PROJECT_NAME} can play music in your group's voice chat as well as channel voice chats
-
+⚪️ {PROJECT_NAME} can play music in your group's voice chat as well as channel voice chats and thanks to 
+@call_me_Vijay1 
 ⚪️ Assistant name >> @{ASSISTANT_NAME}\n\nClick next for instructions**
 """,
         f"""
