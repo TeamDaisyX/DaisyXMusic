a815ae74e1c7d6c9f26d91b4426bc8dfd19a8775
Hunter XDD <85411336+Hunter-XDD@users.noreply.github.com>
2021-10-08 14:04:59 +0530

    Change it to ytdlp

--- DaisyXMusic/services/downloaders/youtube.py
+++ DaisyXMusic/services/downloaders/youtube.py
@@ -18,7 +18,7 @@
 
 from os import path
 
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 
 from DaisyXMusic.config import DURATION_LIMIT
 from DaisyXMusic.helpers.errors import DurationLimitError
