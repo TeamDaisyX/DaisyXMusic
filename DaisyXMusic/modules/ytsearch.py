ed15217fe3ba5c81dbfe47d6d749b76b375d8257
Hunter XD <85411336+Hunter-XD@users.noreply.github.com>
2022-02-05 09:54:17 +0530

    umph

--- DaisyXMusic/modules/ytsearch.py
+++ DaisyXMusic/modules/ytsearch.py
@@ -1,38 +1,10 @@
-# Daisyxmusic (Telegram bot project )
-# Copyright (C) 2021  Inukaasith
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
-# the logging things
-import logging
-
+from pyrogram import filters
 from pyrogram import Client as app
 from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger(__name__)
 
-import pyrogram
-
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-
-@app.on_message(pyrogram.filters.command(["search"]))
+@app.on_message(filters.command(["search"]))
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
@@ -40,7 +12,7 @@ async def ytsearch(_, message: Message):
             return
         query = message.text.split(None, 1)[1]
         m = await message.reply_text("Searching....")
-        results = YoutubeSearch(query, max_results=4).to_dict()
+        results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
         while i < 4:
