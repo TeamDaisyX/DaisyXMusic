fe1d81005152da633249d97b37e1a0e751f3b010
Hunter XD <85411336+Hunter-XD@users.noreply.github.com>
2022-02-05 09:56:20 +0530

    umph

--- DaisyXMusic/config.py
+++ DaisyXMusic/config.py
@@ -1,27 +1,6 @@
-# DAISYXMUSIC- Telegram bot project
-# Copyright (C) 2021  Roj Serbest
-# Copyright (C) 2021  Inuka Asith
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by Inukaasith
-
 import os
 from os import getenv
-
 from dotenv import load_dotenv
-
-if os.path.exists("local.env"):
-    load_dotenv("local.env")
 
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
