816098ca534fb50089a832926eeffc14f54cb2a3
Hunter XD <85411336+Hunter-XD@users.noreply.github.com>
2022-02-05 09:55:32 +0530

    umph

--- DaisyXMusic/__main__.py
+++ DaisyXMusic/__main__.py
@@ -1,22 +1,6 @@
-# DaisyXMusic (Telegram bot project)
-# Copyright (C) 2021  Inuka Asith & Rojserbest
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 import requests
 from pyrogram import Client as Bot
+from pytgcalls import idle
 
 from DaisyXMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
 from DaisyXMusic.services.callsmusic import run
@@ -36,3 +20,4 @@ bot = Bot(
 
 bot.start()
 run()
+idle
