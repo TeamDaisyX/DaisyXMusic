37944090236e8828d96ce1077980e2f5b578008d
Hunter XD <85411336+Hunter-XD@users.noreply.github.com>
2022-02-11 16:31:55 +0530

    umph

--- str.py
+++ str.py
@@ -7,7 +7,9 @@ print("Enter your app information from my.telegram.org/apps below.")
 
 async def main():
     async with Client(
-        ":memory:", api_id=int(input("API ID:")), api_hash=input("API HASH:")
+        ":memory:", 
+        api_id=int(input("API ID:\n")), 
+        api_hash=input("API HASH:\n"),
     ) as app:
         print(await app.export_session_string())
 
